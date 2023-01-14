2123f854bccc046bd6c53e760a0918932792428b
wibmw <ilaouer@outlook.com>
2023-01-14 17:07:22 +0100

    v0.0.4 sorter fix

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -4,26 +4,20 @@ import TBody from './TBody'
 import TFooter from './TFooter'
 import useSortableTable from '../hook/useSortableTable'
 import usePaginationTable from '../hook/usePaginationTable'
-import { FilterableTable } from '../utils/filter'
 
 const Table = ({ title, tableDatas, columns }: IPropsTable) => {
-  // Table Search Filter
-  const [filter, setFilter] = useState<string>(''),
-    // Table Sorted Datas
-    [sortedDatas, handleSorting] = useSortableTable(tableDatas, columns),
+  // Table Sorted Datas
+  const [sortedDatas, handleSorting, setFilter] = useSortableTable(tableDatas, columns),
     // Table Active Page
     [page, setPage] = useState<number>(1),
     // Table Page Lines Number
     [rowsPerPage, setRowsPerPage] = useState<number>(10),
-    // Table Filtered Datas
-    [filteredData, setFilteredData] = useState(FilterableTable(sortedDatas, filter)),
     // Filtered Datas Slices
-    { slice, range } = usePaginationTable({ datas: filteredData, page, rowsPerPage }),
+    { slice, range } = usePaginationTable({ datas: sortedDatas, page, rowsPerPage }),
     // On Filter Change, update the filter state, filter the sorted datas with the new value
     // Set the active page to 1 to prevent issues
     handleFilterChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
       setFilter(target.value.replace(/[^a-zA-Z0-9 ]/g, ''))
-      setFilteredData(FilterableTable(sortedDatas, filter))
       setPage(1)
     }
 
